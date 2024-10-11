c37b01bc775cd2317e29392cb7f275f23bdbfe41
RoMANzhula <roman1985al@ukr.net>
2024-10-11 22:35:32 +0300

    Initial commit

---
+++ src/test/java/org/romanzhula/kafkademoapp/KafkaDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.romanzhula.kafkademoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
