10f451721d34b0a13a9de0bb23a2b3c4dd1b61e5
RoMANzhula <roman1985al@ukr.net>
2024-10-12 17:07:36 +0300

    Add KafkaDataServiceImpl - reactive (asynchronous) send() method

---
+++ src/main/java/org/romanzhula/kafkademoapp/service/KafkaDataServiceImpl.java
@@ -0,0 +1,40 @@
+package org.romanzhula.kafkademoapp.service;
+
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.kafkademoapp.model.Data;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderRecord;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaDataServiceImpl implements KafkaDataService {
+
+    private final KafkaSender<String, Object> kafkaSender;
+
+    @Override
+    public void send(Data data) {
+        String topic = switch (data.getMeasurementType()) {
+            case POWER -> "data-power";
+            case VOLTAGE -> "data-voltage";
+            case TEMPERATURE -> "data-temperature";
+        };
+
+        kafkaSender
+                .send(
+                        Mono.just(
+                                SenderRecord.create(
+                                        topic,
+                                        0,
+                                        System.currentTimeMillis(),
+                                        String.valueOf(data.hashCode()),
+                                        data,
+                                        null
+                                )
+                        )
+                )
+                .subscribe()
+        ;
+    }
+}
